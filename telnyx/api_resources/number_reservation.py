ea83ef8d8733d28a8dc072638a9b68de44062381
luizanao <luiz@telnyx.com>
2019-07-29 23:19:57 +0100

    Remove unused import

--- telnyx/api_resources/number_reservation.py
+++ telnyx/api_resources/number_reservation.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import, division, print_function
 from telnyx.api_resources.abstract import (
     CreateableAPIResource,
     ListableAPIResource,
-    UpdateableAPIResource,
     nested_resource_class_methods,
 )
 
