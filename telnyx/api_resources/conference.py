83c724a80a903ccdc909597c3cab7de400bb3e44
luizanao <luiz@telnyx.com>
2019-07-31 14:55:29 +0100

    Fix action resource typo

--- telnyx/api_resources/conference.py
+++ telnyx/api_resources/conference.py
@@ -9,7 +9,7 @@ from telnyx.api_resources.abstract import (
 
 @nested_resource_class_methods("join", path="actions/join", operations=["create"])
 @nested_resource_class_methods("mute", path="actions/mute", operations=["create"])
-@nested_resource_class_methods("umute", path="actions/umute", operations=["create"])
+@nested_resource_class_methods("unmute", path="actions/unmute", operations=["create"])
 @nested_resource_class_methods("hold", path="actions/hold", operations=["create"])
 @nested_resource_class_methods("unhold", path="actions/unhold", operations=["create"])
 class Conference(CreateableAPIResource, ListableAPIResource):
@@ -21,8 +21,8 @@ class Conference(CreateableAPIResource, ListableAPIResource):
     def mute(self, **params):
         return Conference.create_mute(self.id, **params)
 
-    def umute(self, **params):
-        return Conference.create_umute(self.id, **params)
+    def unmute(self, **params):
+        return Conference.create_unmute(self.id, **params)
 
     def hold(self, **params):
         return Conference.create_hold(self.id, **params)
