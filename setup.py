94330d60ed780b08c9762b97cf53b068fe78f9c0
eoghan <eoghan@telnyx.com>
2019-08-01 18:03:38 -0500

    Signature validation update

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     install_requires=[
         'requests >= 2.20; python_version >= "3.0"',
         'requests[security] >= 2.20; python_version < "3.0"',
-        "pycryptodome",
+        "PyNaCl",
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     tests_require=[
