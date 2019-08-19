18bf2f804e454dc8e886f9a35ad9ed9ad2e4823b
Nick Timkovich <nick@telnyx.com>
2019-08-19 16:04:54 -0500

    Avoid installing PyTest 5+ on Python 2.x

--- setup.py
+++ setup.py
@@ -52,7 +52,8 @@ setup(
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     tests_require=[
-        "pytest >= 3.4",
+        'pytest >= 4; python_version >= "3.0"',
+        'pytest >= 4, < 5; python_version < "3.0"',
         "pytest-mock >= 1.7",
         "pytest-xdist >= 1.22",
         "pytest-cov >= 2.5",
