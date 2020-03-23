7d6aefe5a7b86284a949022b2c61bf67f64408a9
Kane Mathers <kane@kanemathers.name>
2020-03-23 15:05:47 +0800

    PORTAL-867: Add number order regulatory requirements

---
+++ telnyx/api_resources/phone_number_regulatory_requirement.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, division, print_function
+
+from telnyx import error
+from telnyx.api_resources.abstract import ListableAPIResource
+
+
+class PhoneNumberRegulatoryRequirement(ListableAPIResource):
+    OBJECT_NAME = "phone_number_regulatory_requirement"
+
+    @classmethod
+    def retrieve(cls, id, api_key=None, **params):
+        raise error.InvalidRequestError(
+            "%s does not support retrieve()" % cls.class_url()
+        )
