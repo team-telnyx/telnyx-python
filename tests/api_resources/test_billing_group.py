de93551837b8c99285248fef3bcaeeba2106761c
Kane Mathers <kane@kanemathers.name>
2020-03-27 16:56:45 +0800

    PORTAL-872: Add billing group endpoint

---
+++ tests/api_resources/test_billing_group.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import, division, print_function
+
+import telnyx
+
+TEST_RESOURCE_ID = "123"
+
+
+class TestBillingGroup(object):
+    def test_is_listable(self, request_mock):
+        resources = telnyx.BillingGroup.list()
+        request_mock.assert_requested("get", "/v2/billing_groups")
+        assert isinstance(resources.data, list)
+        assert isinstance(resources.data[0], telnyx.BillingGroup)
+
+    def test_is_retrievable(self, request_mock):
+        resource = telnyx.BillingGroup.retrieve(TEST_RESOURCE_ID)
+        request_mock.assert_requested("get", "/v2/billing_groups/%s" % TEST_RESOURCE_ID)
+        assert isinstance(resource, telnyx.BillingGroup)
+
+    def test_is_creatable(self, request_mock):
+        resource = telnyx.BillingGroup.create(name="foo")
+        request_mock.assert_requested("post", "/v2/billing_groups")
+        assert isinstance(resource, telnyx.BillingGroup)
+
+    def test_is_saveable(self, request_mock):
+        billing_group = telnyx.BillingGroup.retrieve(TEST_RESOURCE_ID)
+        billing_group.name = "bar"
+        resource = billing_group.save()
+        request_mock.assert_requested(
+            "patch", "/v2/billing_groups/%s" % TEST_RESOURCE_ID
+        )
+        assert isinstance(resource, telnyx.BillingGroup)
+        assert resource is billing_group
+
+    def test_is_modifiable(self, request_mock):
+        resource = telnyx.BillingGroup.modify(TEST_RESOURCE_ID, name="foo")
+        request_mock.assert_requested(
+            "patch", "/v2/billing_groups/%s" % TEST_RESOURCE_ID
+        )
+        assert isinstance(resource, telnyx.BillingGroup)
+
+    def test_is_deletable(self, request_mock):
+        resource = telnyx.BillingGroup.retrieve(TEST_RESOURCE_ID)
+        resource.delete()
+        request_mock.assert_requested(
+            "delete", "/v2/billing_groups/%s" % TEST_RESOURCE_ID
+        )
