0f65d8c23f9ddb10bb47cf59b88e015954359984
Kane Mathers <kane@kanemathers.name>
2020-07-14 15:37:54 +0800

    Remove dependency on urllib.parse
    Forgot this needed to be compatible with 2.7

--- tests/api_resources/test_number_lookup.py
+++ tests/api_resources/test_number_lookup.py
@@ -1,16 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
-import urllib.parse
-
 import telnyx
-
-TEST_RESOURCE_ID = "+18665552368"
 
 
 class TestNumberLookup(object):
     def test_is_retrievable(self, request_mock):
-        resource = telnyx.NumberLookup.retrieve(TEST_RESOURCE_ID)
-        request_mock.assert_requested(
-            "get", "/v2/number_lookup/%s" % urllib.parse.quote(TEST_RESOURCE_ID)
-        )
+        resource = telnyx.NumberLookup.retrieve("+18665552368")
+        request_mock.assert_requested("get", "/v2/number_lookup/%2B18665552368")
         assert isinstance(resource, telnyx.NumberLookup)
