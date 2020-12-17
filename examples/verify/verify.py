3951b4c2320ead999f25cdc3a73f5dd93b7e48a5
yiuming <yiuming@telnyx.com>
2020-12-17 09:20:59 -0600

    PORTAL-1679 black the examples!

--- examples/verify/verify.py
+++ examples/verify/verify.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     verify_resp = verification.verify_by_phone_number(
         code=code, phone_number=phone_number
     )
-    if verify_resp["data"]["response_code"] == "accepted":
+    if verify_resp.data.response_code == "accepted":
         print("Verify successful!")
     else:
         print("There was an issue verifying: {}".format(verify_resp["data"]))
--- examples/verify/verify_profiles.py
+++ examples/verify/verify_profiles.py
@@ -7,10 +7,8 @@ if __name__ == '__main__':
 
     telnyx.api_key = os.environ.get("TELNYX_SECRET_KEY")
     res = telnyx.VerifyProfile.create(
-        name=profile_name,
-        messaging_enabled=True,
-        default_timeout_secs=600,
+        name=profile_name, messaging_enabled=True, default_timeout_secs=600
     )
     print(res)
     profiles = telnyx.VerifyProfile.list()
-    print(len(profiles))
+    print(profiles)
