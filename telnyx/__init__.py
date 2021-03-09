39da1b0a81778036966933504d2c045e8a8e22c2
d-telnyx <68876596+d-telnyx@users.noreply.github.com>
2021-03-08 22:19:05 -0500

    bump version (#47)

--- telnyx/__init__.py
+++ telnyx/__init__.py
@@ -24,7 +24,7 @@ public_key = os.environ.get("TELNYX_PUBLIC_KEY")
 log = None
 
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 
 # Sets some basic information about the running application that's sent along
