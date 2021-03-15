3856c103a4c2fd52f24ea047126423606993714f
Quagser <quagmilion@gmail.com>
2021-03-15 08:51:17 -0500

    Added number hosting sans upload documents (need a working example)

---
+++ tests/api_resources/test_messaging_hosted_number_order.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import, division, print_function
+
+import telnyx
+
+TEST_RESOURCE_ID = "6a09cdc3-8948-47f0-aa62-74ac943d6c58"
+
+
+class TestMessagingHostedNumberOrder(object):
+    def test_is_listable(self, request_mock):
+        resources = telnyx.MessagingHostedNumberOrder.list()
+        request_mock.assert_requested("get", "/v2/messaging_hosted_number_orders")
+        assert isinstance(resources.data, list)
+        assert isinstance(resources.data[0], telnyx.MessagingHostedNumberOrder)
+
+    def test_is_retrievable(self, request_mock):
+        resource = telnyx.MessagingHostedNumberOrder.retrieve(TEST_RESOURCE_ID)
+        request_mock.assert_requested("get", "/v2/messaging_hosted_number_orders/%s" % TEST_RESOURCE_ID)
+        assert isinstance(resource, telnyx.MessagingHostedNumberOrder)
+
+    def test_is_creatable(self, request_mock):
+        resource = telnyx.MessagingHostedNumberOrder.create()
+        request_mock.assert_requested("post", "/v2/messaging_hosted_number_orders")
+        assert isinstance(resource, telnyx.MessagingHostedNumberOrder)
+
+    def test_is_deletable(self, request_mock):
+        resource = telnyx.MessagingHostedNumberOrder.retrieve(TEST_RESOURCE_ID)
+        resource.delete()
+        request_mock.assert_requested(
+            "delete", "/v2/messaging_hosted_number_orders/%s" % TEST_RESOURCE_ID
+        )
