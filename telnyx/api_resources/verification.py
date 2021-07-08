4089bc0fe83703f6786b8a765e5deb5f128fc72c
vionash <61507189+vionash@users.noreply.github.com>
2021-07-08 17:14:37 -0500

    updated verify endpoints + tests (#57)
    * updated verify endpoints + tests
    
    * attempt lint fix
    
    Co-authored-by: vionash <vionash@telnyx.com>
    Co-authored-by: Drew Kitch <drew@telnyx.com>

--- telnyx/api_resources/verification.py
+++ telnyx/api_resources/verification.py
@@ -1,6 +1,25 @@
-from telnyx.api_resources.abstract import CreateableAPIResource, ListableAPIResource
+from telnyx.api_resources.abstract import (
+    CreateableAPIResource,
+    ListableAPIResource,
+    nested_resource_class_methods,
+)
 
 
+@nested_resource_class_methods(
+    "sms", path="/v2/verifications/sms", operations=["create"]
+)
+@nested_resource_class_methods(
+    "psd2", path="/v2/verifications/psd2", operations=["create"]
+)
+@nested_resource_class_methods(
+    "call", path="/v2/verifications/call", operations=["create"]
+)
+@nested_resource_class_methods(
+    "flashcall", path="/v2/verifications/flashcall", operations=["create"]
+)
+@nested_resource_class_methods(
+    "whatsapp", path="/v2/verifications/whatsapp", operations=["create"]
+)
 class Verification(CreateableAPIResource, ListableAPIResource):
     OBJECT_NAME = "verification"
 
@@ -12,3 +31,23 @@ class Verification(CreateableAPIResource, ListableAPIResource):
             ),
             params={"code": code},
         )
+
+    @classmethod
+    def sms(cls, **params):
+        return Verification.create_sms(None, **params)
+
+    @classmethod
+    def psd2(cls, **params):
+        return Verification.create_psd2(None, **params)
+
+    @classmethod
+    def call(cls, **params):
+        return Verification.create_call(None, **params)
+
+    @classmethod
+    def flashcall(cls, **params):
+        return Verification.create_flashcall(None, **params)
+
+    @classmethod
+    def whatsapp(cls, **params):
+        return Verification.create_whatsapp(None, **params)
