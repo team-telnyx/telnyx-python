a5b7f7bf29c32aef06aabd4151161ec9572610e2
vladi <vladi@telnyx.com>
2021-12-07 20:39:33 -0600

    incriment in init

--- telnyx/__init__.py
+++ telnyx/__init__.py
@@ -24,7 +24,7 @@ public_key = os.environ.get("TELNYX_PUBLIC_KEY")
 log = None
 
 
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 
 
 # Sets some basic information about the running application that's sent along
