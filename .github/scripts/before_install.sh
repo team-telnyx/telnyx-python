7d2191f39a7fda3fbe2861082d1a8d4fc729e275
VeryKinde <vlad@kinde.com>
2024-02-23 01:54:43 -0600

    fix workflows

--- .github/scripts/before_install.sh
+++ .github/scripts/before_install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Install the necessary tools
+# Abort on errors
+set -e
+
+# Ensure jq and curl are installed
+sudo apt-get update && sudo apt-get install -y jq curl
+
+# Install the necessary tools (specify Node.js version as needed)
 npm install -g @stoplight/prism-cli
 
 # Variable for original and modified OpenAPI spec files
@@ -10,15 +16,15 @@ modified_spec="/tmp/modified_spec.json"
 # Download the OpenAPI spec file
 curl "${TELNYX_MOCK_OPEN_API_URI}" -o "${original_spec}"
 
-# Use a tool like jq to modify the file (you may need to install jq or another JSON processor)
+# Modify the file using jq
 jq '.paths |= with_entries(.key |= "/v2" + .)' "${original_spec}" > "${modified_spec}"
 
 echo "Starting up Prism Mock Server with modified spec file"
-prism mock "${modified_spec}" > /dev/null &
+prism mock "${modified_spec}" > prism.log 2>&1 &
 
 # Your existing setup for the Telnyx Prism Mock (if needed)
 git clone https://github.com/team-telnyx/telnyx-prism-mock.git
 cd telnyx-prism-mock/proxy
 npm install
-node index.js > /dev/null &
+node index.js > proxy.log 2>&1 &
 cd -
