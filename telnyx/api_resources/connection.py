525030b943a7e325fdc95664349029838a0e0dfd
ADandyGuyInSpace <vladi@telnyx.com>
2025-03-21 10:52:05 -0500

    fix typo + inbound mock test

--- telnyx/api_resources/connection.py
+++ telnyx/api_resources/connection.py
@@ -6,7 +6,7 @@ from telnyx.api_resources.abstract import (
 )
 
 
-@nested_resource_class_methods("active_call", path="active_calls", operations=["list"])
+@nested_resource_class_methods("active_calls", path="active_calls", operations=["list"])
 class Connection(ListableAPIResource):
     OBJECT_NAME = "connection"
     
